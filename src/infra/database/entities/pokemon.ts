ab5a3d39867e038062a0fca3fa34874379aaac03
DevLeuzera <le.tex@hotmail.com>
2022-11-01 18:50:55 -0300

    feat: add Pokemon entity

---
+++ src/infra/database/entities/pokemon.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/indent */
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity({ name: 'pokemons' })
+export class Pokemon {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    name: 'tipo'
+  })
+  tipo: string
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+    name: 'treinador'
+  })
+  treinador: string
+
+  @Column({
+    type: 'int',
+    nullable: false,
+    name: 'nivel',
+    default: 1
+  })
+  nivel: number
+}
