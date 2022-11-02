f5912c576a2234abd7ace91c67e8bda5dd410d6e
DevLeuzera <le.tex@hotmail.com>
2022-11-01 22:10:16 -0300

    feat: add find pokemon route

--- src/main/routes/pokemon.ts
+++ src/main/routes/pokemon.ts
@@ -2,6 +2,7 @@ import { adaptRoute } from '@/main/adapters'
 import {
   makeAddPokemonController,
   makeEditPokemonController,
+  makeFindPokemonController,
   makeRemovePokemonController
 } from '@/main/factories/controllers'
 
@@ -11,4 +12,5 @@ export const applyPokemonRoutes = (router: Router): void => {
   router.post('/pokemons', adaptRoute(makeAddPokemonController()))
   router.put('/pokemons/:id', adaptRoute(makeEditPokemonController()))
   router.delete('/pokemons/:id', adaptRoute(makeRemovePokemonController()))
+  router.get('/pokemons/:id', adaptRoute(makeFindPokemonController()))
 }
