5cbf264c85edd431651a851ab1f1d390e3b76432
DevLeuzera <le.tex@hotmail.com>
2022-11-02 01:12:01 -0300

    refactor: update environment variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,10 +1,10 @@
 export const env = {
   port: Number(process.env.PORT) || 3030,
 
-  mssqlHost: process.env.POSTGRES_HOST || 'desafio-jazida.database.windows.net',
-  mssqlPort: Number(process.env.POSTGRES_PORT) || 1433,
-  mssqlUsername: process.env.POSTGRES_USERNAME || 'DesafioAdmin',
-  mssqlPassword: process.env.POSTGRES_PASSWORD || 'Picachu123',
-  mssqlSchema: process.env.POSTGRES_SCHEMA || 'schema-leo',
-  mssqlDatabase: process.env.POSTGRES_DATABASE || 'Desafio-jz'
+  mssqlHost: process.env.MSSQL_HOST || 'desafio-jazida.database.windows.net',
+  mssqlPort: Number(process.env.MSSQL_PORT) || 1433,
+  mssqlUsername: process.env.MSSQL_USERNAME || 'DesafioAdmin',
+  mssqlPassword: process.env.MSSQL_PASSWORD || 'Picachu123',
+  mssqlSchema: process.env.MSSQL_SCHEMA || 'schema-leo',
+  mssqlDatabase: process.env.MSSQL_DATABASE || 'Desafio-jz'
 }
